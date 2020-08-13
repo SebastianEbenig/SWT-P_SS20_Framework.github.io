c22ee642f769b50911f6b573207d06010d488a88
SebastianEbenig <64591089+SebastianEbenig@users.noreply.github.com>
2020-08-13 16:44:58 +0200

    Auto-generated commit

---
+++ docs-docs-module-es5.js
@@ -0,0 +1,137 @@
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["docs-docs-module"], {
+  /***/
+  "./src/app/docs/docs-routing.module.ts":
+  /*!*********************************************!*\
+    !*** ./src/app/docs/docs-routing.module.ts ***!
+    \*********************************************/
+
+  /*! exports provided: DocsPageRoutingModule */
+
+  /***/
+  function srcAppDocsDocsRoutingModuleTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "DocsPageRoutingModule", function () {
+      return DocsPageRoutingModule;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+    /* harmony import */
+
+
+    var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+    /*! @angular/router */
+    "./node_modules/@angular/router/__ivy_ngcc__/fesm2015/router.js");
+    /* harmony import */
+
+
+    var _docs_page__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+    /*! ./docs.page */
+    "./src/app/docs/docs.page.ts");
+
+    var routes = [{
+      path: '',
+      component: _docs_page__WEBPACK_IMPORTED_MODULE_3__["DocsPage"]
+    }];
+
+    var DocsPageRoutingModule = function DocsPageRoutingModule() {
+      _classCallCheck(this, DocsPageRoutingModule);
+    };
+
+    DocsPageRoutingModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+      imports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"].forChild(routes)],
+      exports: [_angular_router__WEBPACK_IMPORTED_MODULE_2__["RouterModule"]]
+    })], DocsPageRoutingModule);
+    /***/
+  },
+
+  /***/
+  "./src/app/docs/docs.module.ts":
+  /*!*************************************!*\
+    !*** ./src/app/docs/docs.module.ts ***!
+    \*************************************/
+
+  /*! exports provided: DocsPageModule */
+
+  /***/
+  function srcAppDocsDocsModuleTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "DocsPageModule", function () {
+      return DocsPageModule;
+    });
+    /* harmony import */
+
+
+    var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! tslib */
+    "./node_modules/tslib/tslib.es6.js");
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+    /* harmony import */
+
+
+    var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+    /*! @angular/common */
+    "./node_modules/@angular/common/__ivy_ngcc__/fesm2015/common.js");
+    /* harmony import */
+
+
+    var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+    /*! @angular/forms */
+    "./node_modules/@angular/forms/__ivy_ngcc__/fesm2015/forms.js");
+    /* harmony import */
+
+
+    var _ionic_angular__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(
+    /*! @ionic/angular */
+    "./node_modules/@ionic/angular/__ivy_ngcc__/fesm2015/ionic-angular.js");
+    /* harmony import */
+
+
+    var _docs_routing_module__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(
+    /*! ./docs-routing.module */
+    "./src/app/docs/docs-routing.module.ts");
+    /* harmony import */
+
+
+    var _docs_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(
+    /*! ./docs.page */
+    "./src/app/docs/docs.page.ts");
+
+    var DocsPageModule = function DocsPageModule() {
+      _classCallCheck(this, DocsPageModule);
+    };
+
+    DocsPageModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+      imports: [_angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"], _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"], _ionic_angular__WEBPACK_IMPORTED_MODULE_4__["IonicModule"], _docs_routing_module__WEBPACK_IMPORTED_MODULE_5__["DocsPageRoutingModule"]],
+      declarations: [_docs_page__WEBPACK_IMPORTED_MODULE_6__["DocsPage"]]
+    })], DocsPageModule);
+    /***/
+  }
+}]);
+//# sourceMappingURL=docs-docs-module-es5.js.map
